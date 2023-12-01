aa5c7ac48d295da3409e8857a0814144c5e69c59
hyunjialee <hyjialee@gmail.com>
2023-11-30 19:52:24 -0500

    webclient class

---
+++ provided/WebClient.java
@@ -0,0 +1,46 @@
+package rocks.zipcode;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Objects;
+import java.util.Scanner;
+
+
+public class WebClient {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String navi;
+
+        do {
+            System.out.print("curl ");
+            navi = scan.nextLine();
+            navi = !navi.isEmpty() ? navi : "/";
+
+            //if statement if line 31 URL is removed
+            // if navi.isEmpty() Sout "Could not resolve host: curl"
+            // 6 - cannot resolve host
+            // 7 - cannot connect
+            // 20 - http cannot be retrieved
+            // ^^^^^^ ERROR MESSAGES from CURL
+
+            HttpClient client = HttpClient.newBuilder() // New client for HTTP
+                    .version(HttpClient.Version.HTTP_2)  //Version HTTP
+                    .followRedirects(HttpClient.Redirect.NORMAL)  // not be redirected from any other webpage
+                    // NORMAL means only GET and HEAD request (not sure the exact meanings)
+                    .proxy(ProxySelector.of(new InetSocketAddress("localhost", 8000))) //define host name with specified host
+                    .build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8000/" + navi)) // using navi to use foo, bar, test
+                    .build();                 //  ^^^^ Can remove URL to imitate CURL command
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenAccept(System.out::println)
+                    .join();
+
+
+        } while (!navi.equals("exit"));
+        scan.close();
+    }
+}
